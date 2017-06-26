de179439443127c2e5262b21ae4cbef9849dbbbe
omkarsheral1989 <omkar.sheral.1989@gmail.com>
2017-06-26 16:54:51 +0530

    some more methods

--- Utils.java
+++ Utils.java
@@ -1,11 +1,31 @@
-package com.sheral.omkar.twguestwifipoc;
+package com.sheral.omkar.twguestwifipoc.utils;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.content.ContextCompat;
 
 public class Utils {
+  private Utils() {
+  }
+
   public static <T> T getSystemService(@NonNull Context context, @NonNull String serviceName) {
     //noinspection unchecked
     return (T) context.getSystemService(serviceName);
+  }
+
+  public static boolean hasPermission(Context context, String permission) {
+    return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+  }
+
+  @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+  public static boolean isConnectedToInternet(Context context) {
+    ConnectivityManager connectivityManager = getSystemService(context, Context.CONNECTIVITY_SERVICE);
+    NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+    return activeNetwork != null && activeNetwork.isConnected();
   }
 }
--- WifiUtils.java
+++ WifiUtils.java
@@ -1,4 +1,4 @@
-package com.sheral.omkar.twguestwifipoc;
+package com.sheral.omkar.twguestwifipoc.utils;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,8 +7,6 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.RequiresPermission;
 
 import com.google.common.base.Optional;
@@ -25,15 +23,21 @@ public class WifiUtils {
   private WifiUtils() {
   }
 
+  /**
+   * Make sure wifi is on
+   */
   public static List<WifiConfiguration> getConfiguredNetworks(Context context) {
     WifiManager wifiManager = getWifiManager(context);
     List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
-    for (WifiConfiguration configuredNetwork : configuredNetworks) {
+    if (configuredNetworks == null) {
+      configuredNetworks = Collections.emptyList();
     }
     return configuredNetworks;
   }
 
-  @Nullable
+  /**
+   * Make sure wifi is on
+   */
   public static Optional<WifiConfiguration> getConfiguredNetwork(Context context, String networkSSID) {
     networkSSID = "\"" + networkSSID + "\"";
     List<WifiConfiguration> configuredNetworks = getConfiguredNetworks(context);
@@ -46,10 +50,14 @@ public class WifiUtils {
   }
 
   /**
-   * @param context
+   * Make sure wifi is on
+   *
    * @return true on success or wifi wasn't configured
    */
   public static boolean forgetConfiguredNetwork(Context context, String networkSSID) {
+    if (!isWifiEnabled(context)) {
+      return false;
+    }
     Optional<WifiConfiguration> configuredNetworkOptional = getConfiguredNetwork(context, networkSSID);
     if (configuredNetworkOptional.isPresent()) {
       WifiConfiguration wifiConfiguration = configuredNetworkOptional.get();
@@ -59,34 +67,71 @@ public class WifiUtils {
     return true;
   }
 
-  public static void configureAndConnectToWifiNetwork(Context context,
-                                                      String networkSSID,
-                                                      String password) {
-    WifiConfiguration configuredWifi = configureWifiNetwork(context, networkSSID, password);
-    connectToConfiguredWifiNetwork(context, configuredWifi);
+  /**
+   * Make sure wifi is on. otherwise will fail
+   */
+  public static boolean configureAndConnectToWifiNetwork(Context context,
+                                                         String networkSSID,
+                                                         String password) {
+    Optional<WifiConfiguration> configuredWifiOpt = configureWifiNetwork(context, networkSSID, password);
+    if (!configuredWifiOpt.isPresent()) {
+      return false;
+    }
+    return connectToConfiguredWifiNetwork(context, configuredWifiOpt.get());
   }
 
+  /**
+   * Make sure wifi is on.
+   *
+   * @return absent on failure
+   */
   @RequiresPermission(CHANGE_WIFI_STATE)
-  @NonNull
-  public static WifiConfiguration configureWifiNetwork(Context context,
-                                                       String networkSSID,
-                                                       String password) {
+  public static Optional<WifiConfiguration> configureWifiNetwork(Context context,
+                                                                 String networkSSID,
+                                                                 String password) {
     WifiConfiguration conf = new WifiConfiguration();
     conf.SSID = "\"" + networkSSID + "\"";   // note the quotes. String should contain SSID in quotes
     conf.preSharedKey = "\"" + password + "\"";
 
     WifiManager wifiManager = getWifiManager(context);
-    wifiManager.addNetwork(conf);
+    int res = wifiManager.addNetwork(conf);
 
-    return conf;
+    final int FAILURE = -1;
+    if (res == FAILURE) {
+      return Optional.absent();
+    }
+
+    conf.networkId = res;
+
+    return Optional.of(conf);
   }
 
-  public static void connectToConfiguredWifiNetwork(Context context,
-                                                    WifiConfiguration configuredWifiNetwork) {
+  /**
+   * Make sure wifi is on.
+   */
+  public static boolean connectToConfiguredWifiNetwork(Context context,
+                                                       WifiConfiguration configuredWifiNetwork) {
     WifiManager wifiManager = getWifiManager(context);
-    wifiManager.disconnect();
-    wifiManager.enableNetwork(configuredWifiNetwork.networkId, true);
-    wifiManager.reconnect();
+    boolean res = wifiManager.disconnect();
+    if (!res) {
+      return false;
+    }
+    res = wifiManager.enableNetwork(configuredWifiNetwork.networkId, true);
+    if (!res) {
+      return false;
+    }
+    res = wifiManager.reconnect();
+    return res;
+  }
+
+  public static boolean isWifiEnabled(Context context) {
+    WifiManager wifiManager = getWifiManager(context);
+    return wifiManager.isWifiEnabled();
+  }
+
+  public static void setWifiState(Context context, boolean enabled) {
+    WifiManager wifiManager = getWifiManager(context);
+    wifiManager.setWifiEnabled(enabled);
   }
 
   public interface WifiScanResultListener {
