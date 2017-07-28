38f9f04a8c5bb79f6c1b6c3995d5c8a79161a0fc
omkarsheral1989 <omkar.sheral.1989@gmail.com>
2017-07-28 15:30:27 +0530

    Update Utils.java: added hasPermissions()

--- Utils.java
+++ Utils.java
@@ -23,6 +23,15 @@ public class Utils {
   public static boolean hasPermission(Context context, String permission) {
     return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
   }
+  
+  public static boolean hasPermissions(Context context, String... permissions) {
+    for (String permission : permissions) {
+      if (!hasPermission(context, permission)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
   public static boolean isConnectedToInternet(Context context) {
